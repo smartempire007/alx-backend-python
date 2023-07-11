e727fe2aa20592948cb2cc35db5e07f8bbfb40c0
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-11 03:37:08 +0100

    fix task 3

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-'''from the previous file, import wait_n into 2-measure_runtime.py.
+'''Import wait_random from 0-basic_async_syntax.
 
-Create a measure_time function with integers n and max_delay as
-arguments that measures the total execution time for wait_n(n, max_delay),
-and returns total_time / n. Your function should return a float.
+Write a function (do not create an async function, use the regular
+function syntax to do this) task_wait_random that takes an integer
+max_delay and returns a asyncio.Task.'''
 
-Use the time module to measure an approximate elapsed time.
-'''
 import asyncio
 import time
 
@@ -14,10 +12,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    '''Measures the total execution time for wait_n(n, max_delay),
-    and returns total_time / n.
-    '''
-    start_time = time.time()
+    """Measures the total execution time for
+    `wait_n(n, max_delay)`, and returns `total_time / n`."""
+    s: float = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    total_time = time.time() - start_time
-    return total_time / n
+    return (time.perf_counter() - s) / n
