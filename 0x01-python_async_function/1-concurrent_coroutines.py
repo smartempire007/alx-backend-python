e1f2a3bc40bbe157ca94b9573101a936828edf18
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-11 02:43:22 +0100

    fix task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,7 @@ The list of the delays should be in ascending order without using
 sort() because of concurrency.
 '''
 import asyncio
+import itertools as it
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -16,11 +17,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> list[float]:
     '''Spawn wait_random n times with the specified max_delay.
     '''
-    delay_list = []
-    i = 0
-
-    while i < n:
-        delay_list.append(await wait_random(max_delay))
-        i += 1
-
-    return sorted(delay_list)
+    delay = await asyncio.gather(*(it.repeat(wait_random(max_delay), n)))
+    return list(delay)
