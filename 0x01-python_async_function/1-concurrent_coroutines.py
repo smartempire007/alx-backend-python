476a2adaafc9833341222933c75fbf37249f18f7
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-11 03:31:41 +0100

    fix on task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-'''Import wait_random from the previous python file that you’ve 
-written and write an async routine called wait_n that takes in 2 
-int arguments (in this order): n and max_delay. You will spawn 
-wait_random n times with the specified max_delay.
+'''Import wait_random from previous python file that you’ve written
+and write an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. You will spawn wait_random n times
+with the specified max_delay.
 
-wait_n should return the list of all the delays (float values). 
-The list of the delays should be in ascending order without using 
-sort() because of concurrency.
-'''
+wait_n should return the list of all the delays (float values). The list
+of the delays should be in ascending order without using sort() because
+of concurrency.'''
+
 import asyncio
-import itertools as it
+import random
 from typing import List
+import itertools as it
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-    '''Spawn wait_random n times with the specified max_delay.
-    '''
-    delay = await asyncio.gather(*(it.repeat(wait_random(max_delay), n)))
-    return list(delay)
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ return the list of all the delays (float values)
+    generated from wait_random"""
+    resp = await asyncio.gather(*(it.repeat(wait_random(max_delay), n)))
+    return list(resp)
